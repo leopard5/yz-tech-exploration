dea943d7b604e1febdd4f27d6e4a750d05904943
qiyazhong <qiyazhong@163.com>
2017-08-10 23:16:30 +0800

    custom annotation

---
+++ src/main/java/com/yz/jvm/annotation/CustomLog.java
@@ -0,0 +1,11 @@
+package com.yz.jvm.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CustomLog {
+
+
+}
