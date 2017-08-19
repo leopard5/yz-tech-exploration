195455caa2d81868ad57ef230358c8672d75ba4e
qiyazhong <qiyazhong@163.com>
2017-08-19 10:17:12 +0800

    forkjoin code example

---
+++ src/main/java/com/yz/jvm/forkjoin/threads/BasicThread.java
@@ -0,0 +1,18 @@
+package com.yz.jvm.forkjoin.threads;
+
+public class BasicThread {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(">>> I am running in a separate thread!");
+            }
+        };
+
+        thread.start();
+        thread.join();
+
+    }
+}
