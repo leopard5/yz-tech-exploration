1073298980916df1617ef463bea5e728e856239a
qiyazhong <qiyazhong@163.com>
2017-09-04 14:15:15 +0800

    // 1.BeanCopier // 2.org.apache.commons.beanutils.BeanUtils // BeanUtils.copyProperties VS PropertyUtils.copyProperties // 3.Dozer

--- src/main/java/com/yz/jvm/copyproperties/TestConvertor.java
+++ src/main/java/com/yz/jvm/copyproperties/TestConvertor.java
@@ -1,9 +1,19 @@
 package com.yz.jvm.copyproperties;
 
+import com.alibaba.fastjson.JSON;
 import com.yz.jvm.test.dto.TestReq;
 import com.yz.jvm.test.dto.TestVo;
+import org.apache.commons.beanutils.BeanUtils;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.cglib.beans.BeanCopier;
 
+import java.lang.reflect.InvocationTargetException;
+
+// 1.BeanCopier
+// 2.org.apache.commons.beanutils.BeanUtils
+// BeanUtils.copyProperties VS PropertyUtils.copyProperties
+// 3.Dozer
 public class TestConvertor {
     private static final BeanCopier TEST_REQ = BeanCopier.create(TestVo.class, TestReq.class, false);
 
@@ -16,11 +26,47 @@ public class TestConvertor {
         return target;
     }
 
-    public static void testProperiesCopy(){
-        // 1.BeanCopier
-        // 2.org.apache.commons.beanutils.BeanUtils
-        // 3.Dozer
+    public static void testBeanCopy() {
+        TestVo vo = new TestVo();
+        vo.setName("abc");
+
+        TestReq target = toTestReq(vo);
+        System.out.println(JSON.toJSONString(target));
+    }
+
+    public static void testBeanUtils() {
+        try {
+            TestVo vo = new TestVo();
+            vo.setName("abc");
+
+            TestReq target = new TestReq();
+            BeanUtils.copyProperties(target, vo);
+
+            System.out.println(JSON.toJSONString(target));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+    }
+
+    public static void testBeanMapper() {
+        Mapper mapper = new DozerBeanMapper();
+
+        TestVo vo = new TestVo();
+        vo.setName("abc");
+
+        TestReq target = mapper.map(vo,TestReq.class);
+
+        System.out.println(JSON.toJSONString(target));
+    }
 
 
+    public static void main(String[] args) {
+        testBeanCopy();
+        testBeanUtils();
+        testBeanMapper();
     }
 }
--- src/main/java/com/yz/jvm/java8/stream/Optional1.java
+++ src/main/java/com/yz/jvm/java8/stream/Optional1.java
@@ -1,5 +1,8 @@
 package com.yz.jvm.java8.stream;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
 import java.util.Optional;
 
 public class Optional1 {
@@ -12,6 +15,9 @@ public class Optional1 {
         optional.orElse("fallback");    // "bam"
 
         optional.ifPresent((s) -> System.out.println(s.charAt(0)));     // "b"
+
+        List<String> names = Lists.newArrayList();
+        names.stream().forEach(System.out::println);
     }
 
 }
