cee5088e1b4adff8ecfd0591181fce43298be8ce
qiyazhong <qiyazhong@163.com>
2017-09-25 22:40:01 +0800

    Java Reflection(一):Java反射指南 Java Reflection(二)：Classes Java Reflection(三):构造器 Java Reflection(四):变量 Java Reflection(五):方法 Java Reflection(六):Getters and Setters Java Reflection(七):私有变量和私有方法 Java Reflection(八):注解 Java Reflection(九):泛型 Java Reflection(十):数组 Java Reflection(十一):动态代理 Java Reflection(十二):动态类加载与重载

--- src/main/java/com/yz/jvm/reflection/Reflect.java
+++ src/main/java/com/yz/jvm/reflection/Reflect.java
@@ -2,22 +2,79 @@ package com.yz.jvm.reflection;
 
 import com.yz.jvm.proxy.Count;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class Reflect {
     public static void main(String[] args) {
-        Method[] methods = Count.class.getMethods();
-
+        Class aClass = Count.class;
+        Method[] methods = aClass.getMethods();
         for (Method method : methods) {
+            Class[] parameterTypes = method.getParameterTypes();
+            method.getReturnType();
             System.out.println("method = " + method.getName());
+
+            // 获取方法注解
+            Annotation[] annotations = method.getDeclaredAnnotations();
+            for(Annotation annotation : annotations){
+
+            }
+
+            // 获取方法参数注解
+            Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+            Class[] parameterTypesAA = method.getParameterTypes();
+
+
         }
 
 
-        Class aClass = Count.class; //获取Class对象
         Constructor[] constructors = aClass.getConstructors();
+
+        Field[] fields = aClass.getFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field.getName() + ",type=" + field.getType());
+
+            // 获取字段注解
+            Annotation[] annotations = field.getDeclaredAnnotations();
+            for(Annotation annotation : annotations) {
+            }
+
+
+//            Annotation annotation = field.getAnnotation(Count.class);
+
+
+
+        }
+
+
+        Annotation[] annotations = aClass.getAnnotations();
+        for (Annotation annotation : annotations) {
+        }
 
 
     }
+
+    public static void printGettersSetters(Class aClass) {
+        Method[] methods = aClass.getMethods();
+        for (Method method : methods) {
+            if (isGetter(method)) System.out.println("getter: " + method);
+            if (isSetter(method)) System.out.println("setter: " + method);
+        }
+    }
+
+
+    public static boolean isGetter(Method method) {
+        if (!method.getName().startsWith("get")) return false;
+        if (method.getParameterTypes().length != 0) return false;
+        return !void.class.equals(method.getReturnType());
+    }
+
+    public static boolean isSetter(Method method) {
+        if (!method.getName().startsWith("set")) return false;
+        return method.getParameterTypes().length == 1;
+    }
+
 
 }
