4518f0a614a9c557eaf14bb00e54b66965667d62
qiyazhong <qiyazhong@163.com>
2017-09-30 23:54:37 +0800

    format code with reflect

--- src/main/java/com/yz/jvm/reflection/Reflect.java
+++ src/main/java/com/yz/jvm/reflection/Reflect.java
@@ -9,8 +9,8 @@ import java.lang.reflect.Method;
 
 public class Reflect {
     public static void main(String[] args) {
-        Class aClass = Count.class;
-        Method[] methods = aClass.getMethods();
+        Class objClass = Count.class;
+        Method[] methods = objClass.getMethods();
         for (Method method : methods) {
             Class[] parameterTypes = method.getParameterTypes();
             method.getReturnType();
@@ -18,26 +18,26 @@ public class Reflect {
 
             // 获取方法注解
             Annotation[] annotations = method.getDeclaredAnnotations();
-            for(Annotation annotation : annotations){
+            for (Annotation annotation : annotations) {
             }
 
             // 获取方法参数注解
             Annotation[][] parameterAnnotations = method.getParameterAnnotations();
             Class[] parameterTypesAA = method.getParameterTypes();
         }
-        Constructor[] constructors = aClass.getConstructors();
-        Field[] fields = aClass.getFields();
+        Constructor[] constructors = objClass.getConstructors();
+        Field[] fields = objClass.getFields();
         for (Field field : fields) {
             System.out.println("field = " + field.getName() + ",type=" + field.getType());
 
             // 获取字段注解
             Annotation[] annotations = field.getDeclaredAnnotations();
-            for(Annotation annotation : annotations) {
+            for (Annotation annotation : annotations) {
             }
 
 //            Annotation annotation = field.getAnnotation(Count.class);
         }
-        Annotation[] annotations = aClass.getAnnotations();
+        Annotation[] annotations = objClass.getAnnotations();
         for (Annotation annotation : annotations) {
         }
     }
