a9543261e144f307248b550868207ac616d5aebe
qiyazhong <qiyazhong@163.com>
2017-10-11 20:07:50 +0800

    netty protocol file

---
+++ src/main/java/com/yz/jvm/netty/protocol/file/FileServerHandler.java
@@ -0,0 +1,54 @@
+package com.yz.jvm.netty.protocol.file;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.DefaultFileRegion;
+import io.netty.channel.FileRegion;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+
+public class FileServerHandler extends SimpleChannelInboundHandler<String> {
+
+    private static final String CR = System.getProperty("line.separator");
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * io.netty.channel.SimpleChannelInboundHandler#messageReceived(io.netty
+     * .channel.ChannelHandlerContext, java.lang.Object)
+     */
+    public void messageReceived(ChannelHandlerContext ctx, String msg)
+            throws Exception {
+        File file = new File(msg);
+        if (file.exists()) {
+            if (!file.isFile()) {
+                ctx.writeAndFlush("Not a file : " + file + CR);
+                return;
+            }
+            ctx.write(file + " " + file.length() + CR);
+            RandomAccessFile randomAccessFile = new RandomAccessFile(msg, "r");
+            FileRegion region = new DefaultFileRegion(
+                    randomAccessFile.getChannel(), 0, randomAccessFile.length());
+            ctx.write(region);
+            ctx.writeAndFlush(CR);
+            randomAccessFile.close();
+        } else {
+            ctx.writeAndFlush("File not found: " + file + CR);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * io.netty.channel.ChannelHandlerAdapter#exceptionCaught(io.netty.channel
+     * .ChannelHandlerContext, java.lang.Throwable)
+     */
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
