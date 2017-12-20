a0b611f381a197498ce01ad157b189d83162b79d
qiyazhong <qiyazhong@163.com>
2017-12-20 20:48:40 +0800

    java8 collect and group by after stream ops

---
+++ src/main/java/com/yz/jvm/data/TestData.java
@@ -0,0 +1,31 @@
+package com.yz.jvm.data;
+
+public class TestData {
+    private Byte type;
+    private Integer id;
+    private String name;
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/yz/jvm/java8/stream/TestStream.java
+++ src/main/java/com/yz/jvm/java8/stream/TestStream.java
@@ -1,28 +1,43 @@
 package com.yz.jvm.java8.stream;
 
 
-import akka.dispatch.Foreach;
+import com.yz.jvm.data.TestData;
+import scala.util.control.Exception;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TestStream {
     public static void main(String[] args) {
-        List<StreamTestBean> beanList = new ArrayList<>();
+        List<TestData> beanList = new ArrayList<TestData>();
 
-        StreamTestBean bean1 = new StreamTestBean();
-        bean1.setName("aa");
+        TestData bean1 = new TestData();
+        bean1.setType((byte)2);
+        bean1.setId(123);
 
-        StreamTestBean bean2 = new StreamTestBean();
-        bean2.setName("bb");
+        TestData bean2 = new TestData();
+        bean2.setType((byte)2);
+        bean2.setId(124);
 
-        StreamTestBean bean3 = new StreamTestBean();
-        bean3.setName("cc");
+        TestData bean3 = new TestData();
+        bean3.setType((byte)3);
+        bean3.setId(888);
 
         beanList.add(bean1);
         beanList.add(bean2);
         beanList.add(bean3);
 
-        StringBuilder sb = new StringBuilder(100);
+        List<Integer> collect1 = beanList.stream().filter((s) -> s.getType().equals(Byte.valueOf((byte) 2))).map((s) -> s.getId()).collect(Collectors.toList());
+        List<Integer> collect2 = beanList.stream().filter((s) -> s.getType().equals(Byte.valueOf((byte) 3))).map((s) -> s.getId()).collect(Collectors.toList());
+        System.out.println(collect1.toString());
+        System.out.println(collect2.toString());
+
+        Map<Byte, Long> collect = beanList.stream().collect(Collectors.groupingBy(TestData::getType, Collectors.counting()));
+        System.out.println(collect);
+
+        Map<Byte, List<TestData>> collect3 = beanList.stream().collect(Collectors.groupingBy(TestData::getType));
+        System.out.println(collect3);
     }
 }
