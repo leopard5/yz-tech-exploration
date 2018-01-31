5cedc026f7c16404dc9ca108bb399493627eea0d
qiyazhong <qiyazhong@163.com>
2018-01-31 23:33:35 +0800

    session test

---
+++ src/main/java/com/yz/jvm/zookeeper/Session_Test.java
@@ -0,0 +1,50 @@
+package com.yz.jvm.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Session_Test implements Watcher {
+    final static String SERVER_LIST = "localhost:2182";
+
+    static ZooKeeper zookeeper = null;
+
+    public static void main(String[] args) {
+
+        try {
+            zookeeper = new ZooKeeper(SERVER_LIST, 20000, new Session_Test());
+            while (zookeeper.getState() != ZooKeeper.States.CONNECTED) {
+                Thread.sleep(3000);
+                System.out.println("...");
+            }
+
+            System.out.println("完成数据获取：" + zookeeper.setData("/test", "test".getBytes(), -1));
+            Thread.sleep(10000000L);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    static void priint_session(ZooKeeper zookeeper) {
+
+        System.out.println("zookeeper: " + zookeeper);
+        System.out.println("sessionId: " + zookeeper.getSessionId());
+        System.out.println("pwd: " + print_byte(zookeeper.getSessionPasswd()));
+
+    }
+
+    static String print_byte(byte[] b) {
+        StringBuilder sb = new StringBuilder("[");
+        for (byte _b : b) {
+            sb.append(_b).append(",");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        System.out.println(event.getState());
+        System.out.println(event.getType());
+    }
+}
