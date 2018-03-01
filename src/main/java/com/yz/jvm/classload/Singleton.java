3b1f3e0d8fc550e5fe77c5ab2e0851a708a58925
qiyazhong <qiyazhong@163.com>
2018-03-01 18:30:01 +0800

    ClassLoad test

---
+++ src/main/java/com/yz/jvm/classload/Singleton.java
@@ -0,0 +1,25 @@
+package com.yz.jvm.classload;
+
+public class Singleton {
+//    private static Singleton singleton = new Singleton();
+
+    public static int count1;
+    public static int count2 = 0;
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        count1++;
+        count2++;
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println("count1=" + count1);
+        System.out.println("count2=" + count2);
+    }
+}
