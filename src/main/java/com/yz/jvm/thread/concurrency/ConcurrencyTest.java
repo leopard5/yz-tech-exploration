cbfa1abd1f3cf61c31cf89497564cf67bf42aa5f
qiyazhong <qiyazhong@163.com>
2018-04-19 21:59:31 +0800

    concurrency and counter

---
+++ src/main/java/com/yz/jvm/thread/concurrency/ConcurrencyTest.java
@@ -0,0 +1,48 @@
+package com.yz.jvm.thread.concurrency;
+
+public class ConcurrencyTest {
+
+    private static final long count = 10000l;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+                System.out.println(a);
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        thread.join();
+        long time = System.currentTimeMillis() - start;
+        System.out.println("concurrency :" + time + "ms,b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
+    }
+
+}
