516d63d056a2788c8c6814ce749357bd462c7d6f
qiyazhong <qiyazhong@163.com>
2018-07-20 22:38:26 +0800

    AtomicStampedReferenceDemo

---
+++ src/main/java/com/yz/jvm/thread/atomic/AtomicStampedReferenceDemo.java
@@ -0,0 +1,23 @@
+package com.yz.jvm.thread.atomic;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class AtomicStampedReferenceDemo {
+    public static void main(String[] args) {
+
+        String str1 = "aaa";
+        String str2 = "bbb";
+        AtomicStampedReference<String> reference = new AtomicStampedReference<String>(str1, 1);
+        reference.compareAndSet(str1, str2, reference.getStamp(), reference.getStamp() + 1);
+        System.out.println("reference.getReference() = " + reference.getReference());
+
+        boolean b = reference.attemptStamp(str2, reference.getStamp() + 1);
+        System.out.println("b: " + b);
+        System.out.println("reference.getStamp() = " + reference.getStamp());
+
+        boolean c = reference.weakCompareAndSet(str2, "ccc", 4, reference.getStamp() + 1);
+        System.out.println("reference.getReference() = " + reference.getReference());
+        System.out.println("c = " + c);
+    }
+
+}
